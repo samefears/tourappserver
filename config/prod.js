2665e9b84b6ffb8ea9ead6a72b5da652b84880bf
drewminns <drew.minns@me.com>
2018-08-03 22:00:20 -0400

    Adding Production Setup

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,9 @@
 module.exports = {
-  mongoDBURI: process.env.MONGODBURI,
+  DB: {
+    URI: process.env.URI,
+    name: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    pass: process.env.DB_PASS
+  },
+  PORT: process.env.PORT
 };
