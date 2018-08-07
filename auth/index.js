d8d5b6d42fbeab7b7b3b7864aa507b4f9fa0190c
drewminns <drew.minns@me.com>
2018-08-06 22:04:25 -0400

    Adds #1 - Login System with JWT

---
+++ auth/index.js
@@ -0,0 +1,54 @@
+const passport = require('passport');
+const User = require('../models/user');
+const { LOGIN_SECRET } = require('../config');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
+
+const localoptions = { usernameField: 'email', passwordField: 'password' };
+
+const localLogin = new LocalStrategy(localoptions, function(email, password, done) {
+  User.findOne({ email: email }, function(err, user) {
+    if (err) {
+      return done(err);
+    }
+
+    if (!user) {
+      return done(null, false);
+    }
+
+    user.comparePassword(password, function(err, isMatch) {
+      if (err) {
+        return done(err);
+      }
+
+      if (!isMatch) {
+        return done(null, false);
+      }
+
+      return done(null, user);
+    });
+  });
+});
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+  secretOrKey: LOGIN_SECRET,
+};
+
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
+  User.findById(payload.sub, function(err, user) {
+    if (err) {
+      return done(err);
+    }
+
+    if (user) {
+      done(null, user);
+    } else {
+      done(null, false);
+    }
+  });
+});
+
+passport.use(jwtLogin);
+passport.use(localLogin);
--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const passport = require('passport');
+const cors = require('cors');
 
 const { PORT } = require('./config');
-const { checkForAuth } = require('./middlewares');
 
 const app = express();
 
 require('./models/db');
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 if (process.env.NODE_ENV === 'production') {
   app.use(morgan('short'));
@@ -18,9 +22,23 @@ if (process.env.NODE_ENV === 'production') {
   app.use(morgan('dev'));
 }
 
+const Authentication = require('./controllers/auth');
 const routes = require('./routes');
 
-app.use('/api', checkForAuth, routes);
+app.use('/api', Authentication.protectRoute, routes);
+
+/**
+ * Authentication Routes
+ */
+
+require('./auth');
+const requireSignin = passport.authenticate('local', { session: false });
+
+app.post('/signin', requireSignin, Authentication.signin);
+app.post('/signup', Authentication.signup);
+
+app.post('/login/facebook', Authentication.signinFB);
+app.post('/login/google', Authentication.signinGoogle);
 
 /* eslint-disable-next-line no-console */
 app.listen(PORT, () => console.log(`Server running on http://locahost:${PORT}`));
