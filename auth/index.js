dd255e426ecd13e69e3c8c891b61caca865f3978
drewminns <drew.minns@me.com>
2018-08-07 22:50:37 -0400

    Another issue

--- auth/index.js
+++ auth/index.js
@@ -1,5 +1,5 @@
 const passport = require('passport');
-const User = require('../models/user');
+const User = require('../models/User');
 const { LOGIN_SECRET } = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
